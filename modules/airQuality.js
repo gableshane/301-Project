4f09d00a839d03f62a10fd62279fb87e064f73ca
Shane Gable <gableshane@gmail.com>
2019-12-25 13:28:46 -0800

    functionality and styling

--- modules/airQuality.js
+++ modules/airQuality.js
@@ -1,3 +1,18 @@
-// SHANE
+'use strict';
 
-// TELL JOSHUA THE FORMAT YOU NEED LOCATION 
+const superagent = require('superagent');
+
+const AQ_API_KEY = process.env.AQ_API_KEY;
+const lat = 47.60; // test data
+const lon = 122.33; // test data
+
+const getAirQuality = function (req , res ) {
+  superagent.get(``).then( airData => { // NEED API URL...
+    console.log(airData);
+  }).catch( () => {
+    console.log('The getAirQuality API call failed.');
+  })
+  res.render('../public/views/pages/results.ejs');
+}
+
+module.exports = getAirQuality;
--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.use(methodOverride('_method'));
 // GLOBAL VARIABLES
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const DATABASE_URL = process.env.DATABASE_URL;
+const AQ_API_KEY = process.env.AQ_API_KEY;
 
-// CALLBACK FUNCTIONS:
-const renderHome = (req , res ) => {
-  res.render('../public/views/pages/index.ejs');
-}
+// IMPORT MODULES
+const renderHome = require('./modules/home.js');
+const getAirQuality = require('./modules/airQuality.js');
 
 
 //DATABASE
@@ -32,11 +32,9 @@ client.connect();
 
 app.get('/', renderHome); // SHANE
 // app.get('/location', getLocation); // JOSHUA
-// app.get('/airQuality', airQuality); // SHANE
+app.get('/airQuality', getAirQuality); // SHANE
 // app.get('/restaurants', getRestaurants); // CRYSTAL
 // app.get('/crime', getCrime); // DAESY
-
-
 
 
 // SERVER LISTENS
