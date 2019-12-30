3cbff1d7538b4bc6429bdb691cc1accb0ce0084b
jjdeforrest <jjdeforrest@users.noreply.github.com>
2019-12-30 10:42:08 -0800

    udpateonme

--- modules/location.js
+++ modules/location.js
@@ -3,49 +3,8 @@
 'use strict';
 
 const superagent = require('superagent');
-const pg = require('pg');
-require('dotenv').config();
-const client = new pg.Client(process.env.DATABASE_URL);
-client.on('error', error => console.error(error));
-client.connect();
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 
-function Location(city, geoData){
-  this.search_query = city;
-  this.formatted_query = geoData.formatted_address;
-  this.latitude = geoData.geometry.location.lat;
-  this.longitude = geoData.geometry.location.lng;
-}
-
-const getLocation = function (request, response) {
-  const tableName = 'locations';
-  const fieldName = 'search_query';
-  const locationHandler = {
-    query: request.query.data,
-    cacheHit: (results) => {
-      response.send(results.rows[0]);
-    },
-    cacheMiss: () => {
-      fetchLocation(request.query.data)
-        .then(data => response.send(data));
-    },
-  };
-  checkDuplicate(locationHandler, tableName, fieldName);
-};
-
-function checkDuplicate(handler, tableName, fieldName) {
-  const SQL = `SELECT * FROM ${tableName} WHERE ${fieldName}=$1`;
-  const values = [handler.query];
-  return client.query( SQL, values )
-    .then(results => {
-      if(results.rowCount > 0) {
-        handler.cacheHit(results);
-      }
-      else {
-        handler.cacheMiss();
-      }
-    })
-    .catch(console.error);
-}
 
 function fetchLocation(query) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
@@ -59,17 +18,5 @@ function fetchLocation(query) {
     )
   })
 }
-
-Location.prototype.saveDB = function() {
-  let SQL = `
-    INSERT INTO locations
-      (search_query,formatted_query,latitude,longitude) 
-      VALUES($1,$2,$3,$4) 
-      RETURNING id
-  `;
-  let values = Object.values(this);
-  return client.query(SQL,values);
-};
-
 
 module.exports = getLocation;
--- server.js
+++ server.js
@@ -1,6 +1,50 @@
-const express = require('express');
-const app = express();
 
+// DEPENDENCIES
+const ejs = require('ejs');
+const pg = require('pg');
+const cors = require('cors');
+const express = require('express');
+const superagent = require('superagent');
+const app = express();
+const methodOverride = require('method-override');
+require('dotenv').config();
+
+// CONFIG
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+// GLOBAL VARIABLES
+const PORT = process.env.PORT || 3000;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const DATABASE_URL = process.env.DATABASE_URL;
+const CENSUS_API_KEY = process.env.CENSUS_API_KEY;
+
+// IMPORT MODULES
+const renderHome = require('./modules/home.js');
+const getMeme = require('./modules/getMeme.js');
+const getPoverty = require('./modules/poverty.js');
+const getAirQuality = require('./modules/airQuality');
+const getLocation = require('./modules/location');
+
+// DATABASE
+const client = new pg.Client(`${DATABASE_URL}`);
+client.on('error', error => console.error(error));
+client.connect();
+
+// ROUTES
+
+app.get('/', renderHome); // SHANE
+app.get('/location', getLocation); // JOSHUA
+app.get('/airQuality', getAirQuality);
+app.get('/poverty', getPoverty);
+app.get('/meme', getMeme); // SHANE
+// app.get('/restaurants', getRestaurants); // CRYSTAL
+// app.get('/crime', getCrime); // DAESY
+
+// SERVER LISTENS
 app.listen(3000, () => {
   console.log('listening on port 3000');
 });
